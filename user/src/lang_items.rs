a7cbca88a78a2c4ceb69dcb3eb106ee8e9979bb9
TrisuyaN <trisuyan@gmail.com>
2024-03-31 21:53:04 +0800

    add user_lib and some apps

---
+++ user/src/lang_items.rs
@@ -0,0 +1,16 @@
+use super::println;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {} {} {}", 
+            location.file(), 
+            location.line(), 
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    loop {}
+}
---
+++ user/src/syscall.rs
@@ -0,0 +1,26 @@
+use core::arch::asm;
+
+const SYSCALL_WRITE: usize = 64;
+const SYSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, arg: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("x10") arg[0] => ret,
+            in("x11") arg[1],
+            in("x12") arg[2],
+            in("x17") id
+        );
+    }
+    ret
+}
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
