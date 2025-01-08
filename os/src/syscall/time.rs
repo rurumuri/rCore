d04d82060051a62b63bb8e144a5b16b9c853f68e
TrisuyaN <trisuyan@gmail.com>
2025-01-08 20:04:07 +0000

    mm: rewrite sys_get_time

--- os/src/syscall/time.rs
+++ os/src/syscall/time.rs
@@ -1,4 +1,8 @@
-use crate::timer::{self, MICRO_PER_SEC};
+use crate::{
+    mm::page_table::translated_byte_buffer,
+    task::current_user_token,
+    timer::{self, get_time_us, MICRO_PER_SEC},
+};
 
 #[repr(C)]
 pub struct TimeVal {
@@ -6,14 +10,38 @@ pub struct TimeVal {
     pub usec: usize,
 }
 
-pub fn sys_get_time(ts: usize, _tz: usize) -> isize {
-    let usec = timer::get_time_us();
-    let time_val = TimeVal {
-        sec: usec / MICRO_PER_SEC,
-        usec,
+// pub fn sys_get_time(ts: usize, _tz: usize) -> isize {
+//     let usec = timer::get_time_us();
+//     let time_val = TimeVal {
+//         sec: usec / MICRO_PER_SEC,
+//         usec,
+//     };
+//     unsafe {
+//         *(ts as *mut TimeVal) = time_val;
+//     }
+//     0
+// }
+
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    let us = get_time_us();
+    let dst_vec = translated_byte_buffer(
+        current_user_token(),
+        ts as *const u8,
+        core::mem::size_of::<TimeVal>(),
+    );
+    let ref time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
     };
-    unsafe {
-        *(ts as *mut TimeVal) = time_val;
+    let src_ptr = time_val as *const TimeVal;
+    for (idx, dst) in dst_vec.into_iter().enumerate() {
+        let unit_len = dst.len();
+        unsafe {
+            dst.copy_from_slice(core::slice::from_raw_parts(
+                src_ptr.wrapping_byte_add(idx * unit_len) as *const u8,
+                unit_len,
+            ));
+        }
     }
     0
 }
--- user/src/bin/08sleep.rs
+++ user/src/bin/08sleep.rs
@@ -8,15 +8,15 @@ use user_lib::{get_time, yield_};
 
 #[no_mangle]
 fn main() -> i32 {
-    // println!("Test sleep begin!");
-    // let current_timer = get_time();
-    // let wait_for = current_timer + 3000;
-    // while get_time() < wait_for {
-    //     // println!("{}, {}", get_time(), wait_for);
-    //     yield_();
-    // }
-    // println!("Test sleep OK!");
-    // 0
-    println!("sys_get_time unavaliable now");
-    -1
+    println!("Test sleep begin!");
+    let current_timer = get_time();
+    let wait_for = current_timer + 900; // block if greater than or equal 1000
+    while get_time() < wait_for {
+        // println!("{}, {}", get_time(), wait_for);
+        yield_();
+    }
+    println!("Test sleep OK!");
+    0
+    // println!("sys_get_time unavaliable now");
+    // -1
 }
